5309de9e3665fb0242854a9ad8f3afa365f0d295
Nitesh Thakare <nthakare09@gmail.com>
2026-07-30 22:48:14 +0530

    Counter App

--- counter-app.js
+++ counter-app.js
@@ -1,18 +1,61 @@
 const counter = document.getElementById("counter");
-let text = 0;
-counter.innerHTML = text;
+let currentCount = 0;
+counter.innerHTML = currentCount;
+
+const changeColor = function() {
+    if (currentCount == 0) {
+        counter.style.color = "white";
+    } else if (currentCount >= 1) {
+        counter.style.color = "#3fd93f";
+    } else {
+        counter.style.color = "red";
+    }
+}
+
+// Increase or decrease by entered value 
+const number = document.getElementById("number");
+let inpNum;
+number.value = inpNum;
+
+const inputNum = function() {
+    if (inpNum == "") {
+        inpNum = 1;
+    } else {
+        inpNum;
+    }
+}
+
+const sum = function() {
+    inputNum();
+    currentCount = currentCount + inpNum;
+    counter.innerHTML = currentCount;
+    changeColor();
+}
 
 function add() {
-    text++;
-    counter.innerHTML = text;
+    // inputNum();
+    // currentCount += inpNum;
+    currentCount++;
+    counter.innerHTML = currentCount;
+    changeColor();
 }
 
 const reset = function() {
-    text = 0;
-    counter.innerHTML = text;
+    currentCount = 0;
+    counter.innerHTML = currentCount;
+    changeColor();
 }
 
 const reduce = function() {
-    text--;
-    counter.innerHTML = text;
+
+    // the code is for not allowing the number to go below 0 
+    // if (currentCount <= 0) {
+    //     currentCount = 0;
+    // } else {
+    //     currentCount--;
+    // }
+
+    currentCount--;
+    counter.innerHTML = currentCount;
+    changeColor();
 }
